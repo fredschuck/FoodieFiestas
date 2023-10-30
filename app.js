9cb79519ac8e6fe44d0684d28658f5a0456a8977
FredSchuck <fredschuck@gmail.com>
2023-10-30 16:51:07 -0400

    Added Atlas DB URI

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const methodOverride = require('method-override');
 const mainRoutes = require('./routes/mainRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 const mongoose = require('mongoose');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 const db_user = process.env.USER;
 const db_password = process.env.PASS;
-const uri =  `mongodb+srv://${db_user}:${db_password}@cluster0.t02wymf.mongodb.net/nbda-project3`;
+
+const uri =  `mongodb+srv://${db_user}:${db_password}@cluster0.t02wymf.mongodb.net/nbda-project3?retryWrites=true&w=majority`;
 
 app.set('view engine', 'ejs');
 
@@ -31,26 +31,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny')); 
 app.use(methodOverride('_method'));
 
+
 // Routes ***********************
 app.use('/', mainRoutes);
 app.use('/events', eventRoutes);
 
 
 // Error Handling ***************
-// app.use((req, res, next) => {
-//     let err = new Error('The server cannot locate ' + req.url);
-//     err.status = 404;
-//     next(err);
-// });
+app.use((req, res, next) => {
+    let err = new Error('The server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
 
-// app.use((err, req, res, next) => {
-//     if(!err.status) {
-//         err.status = 500;
-//         err.message = ('Internal Server Error');
-//     }
-//     res.status(err.status);
-//     res.render('error', { 
-//         error: err,
-//         cssFileName: 'error',
-//         title: 'Error'});
-// });
+app.use((err, req, res, next) => {
+    if(!err.status) {
+        err.status = 500;
+        err.message = ('Internal Server Error');
+    }
+    res.status(err.status);
+    res.render('error', { 
+        error: err,
+        cssFileName: 'error',
+        title: 'Error'});
+});
