820a77b82067c97f96297efd3a4c82988e74079b
FredSchuck <fredschuck@gmail.com>
2023-11-24 15:48:55 -0500

    Added middleware for sessions and new user routes

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mainRoutes = require('./routes/mainRoutes');
 const eventRoutes = require('./routes/eventRoutes');
+const userRoutes = require('./routes/userRoutes');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 const host = process.env.HOST || 'localhost';
@@ -30,15 +34,33 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     });
 
 // middleware & static files
+app.use(
+    session({
+        secret: "ajFeiRf90aEu9eroejDoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: uri}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny')); 
 app.use(methodOverride('_method'));
 
 
+
 // Routes ***********************
 app.use('/', mainRoutes);
 app.use('/events', eventRoutes);
+app.use('/user', userRoutes);
 
 
 // Error Handling ***************
