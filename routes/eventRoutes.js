a62605b3052155c49ae18e529d3a3423b8887372
FredSchuck <fredschuck@gmail.com>
2023-11-25 09:33:00 -0500

    Added validator for event ID's

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/eventController');
 const {fileUpload} = require('../middleware/fileUpload');
 const {isLoggedIn, isAuthor} = require('../middleware/auth');
+const {validateId} = require('../middleware/validator');
 
 router.get('/', controller.index);
 
@@ -10,12 +11,12 @@ router.get('/new', isLoggedIn, controller.new);
 
 router.post('/', isLoggedIn, fileUpload, controller.create);
 
-router.get('/:id', controller.show);
+router.get('/:id', validateId, controller.show);
 
-router.get('/:id/edit', isLoggedIn, isAuthor, controller.edit);
+router.get('/:id/edit', validateId, isLoggedIn, isAuthor, controller.edit);
 
-router.put('/:id', isLoggedIn, fileUpload, controller.update);
+router.put('/:id', validateId, isLoggedIn, fileUpload, controller.update);
 
-router.delete('/:id', isLoggedIn, controller.delete);
+router.delete('/:id', validateId, isLoggedIn, controller.delete);
 
 module.exports = router;
